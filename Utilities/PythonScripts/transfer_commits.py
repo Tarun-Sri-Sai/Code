d33531bda655057a96029bc679f622e1f3e8c9e9
Tarun-Sri-Sai <tarunsrisaiv03@gmail.com>
2025-02-22 07:58:53 +0530

    Fix transfer_commits.py

--- Utilities/PythonScripts/transfer_commits.py
+++ Utilities/PythonScripts/transfer_commits.py
@@ -15,21 +15,30 @@ def run_command(command, cwd=None):
 
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: python transfer_commits.py <src_repo> <dest_repo>")
+    if len(sys.argv) < 3 or len(sys.argv) > 4:
+        print("Usage: python transfer_commits.py <src_repo> <dest_repo>\n"
+              "                                  [<start_commit>]")
         sys.exit(1)
 
     src_repo = os.path.abspath(sys.argv[1])
     dest_repo = os.path.abspath(sys.argv[2])
+    start_commit = sys.argv[3] if len(sys.argv) == 4 else None
 
     if not os.path.exists(dest_repo):
         run_command(f"git init", dest_repo)
 
     commits = run_command("git rev-list --reverse HEAD", src_repo).split("\n")
 
+    if start_commit:
+        if start_commit in commits:
+            commits = commits[commits.index(start_commit):]
+        else:
+            print(f"Error: Start commit {start_commit} not found.")
+            sys.exit(1)
+
     for commit in commits:
-        run_command(f"git restore --source={commit}", src_repo)
-        commit_message = run_command("git log -1 --pretty=%B", src_repo)
+        run_command(f"git restore --source={commit} --worktree .", src_repo)
+        commit_message = run_command(f"git log -1 --pretty=%B {commit}", src_repo)
 
         for item in os.listdir(src_repo):
             if item == ".git":
@@ -44,7 +53,7 @@ def main():
         run_command("git add -A", dest_repo)
         run_command(f"git commit -m \"{commit_message}\"", dest_repo)
 
-    run_command("git restore --source=HEAD", src_repo)
+    run_command("git restore --source=HEAD --worktree .", src_repo)
 
 
 if __name__ == "__main__":
